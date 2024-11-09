a453521265c963f959144dcae18f1b4e25a492c9
ikoamu <ikoamu@gmail.com>
2024-11-09 15:35:00 +0900

    fix: debug

--- pages/api/notes/[[...slug]].ts
+++ pages/api/notes/[[...slug]].ts
@@ -2,11 +2,13 @@ import fs from 'fs'
 
 export default async function handler(req: any, res: any) {
   const { slug } = req.query
+  console.log('slug', slug)
   const stuff = slug.join('')
   console.log(stuff)
   const uri = decodeURIComponent(slug)
   const prefix = uri.includes('\\') ? '' : '/'
   const path = `${uri}`
+  console.log('path', path)
   try {
     const text = fs.readFileSync(`${path}`, { encoding: 'utf-8' })
     res.end(`${text}`)
